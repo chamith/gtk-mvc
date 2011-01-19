9c67c06806909ded8787df58770e50edac6f7253
Chamith Siriwardena <chamith@gmail.com>
2011-01-19 14:44:23 +0000

    adding

--- src/gtk-mvc/ApplicationContext.cs
+++ src/gtk-mvc/ApplicationContext.cs
@@ -4,7 +4,17 @@ namespace Gtk.Mvc
 {
 	public class ApplicationContext:Hashtable
 	{
-
+		static ApplicationContext _appContext;
+		public static ApplicationContext Current
+		{
+			get
+			{
+				if( _appContext == null)
+					_appContext = new ApplicationContext();
+				
+				return _appContext;
+			}
+		}
 	}
 }
 
